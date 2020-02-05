c2124e70b922d7f328c55421dcb6cffa894fcf44
kevinveld2001 <kevinveldscholte@gmail.com>
2020-02-05 18:05:08 +0100

    made a card

--- scan_app/lib/shop_list.dart
+++ scan_app/lib/shop_list.dart
@@ -15,8 +15,37 @@ class _Shop_listState extends State<Shop_list> {
     return ListView.builder(
         itemCount: widget.list.length,
         itemBuilder:(BuildContext ctxt, int index) {
-          return new Text(widget.list[index]);
+          return Padding(
+          padding: EdgeInsets.all(10),
+          child: Card(
+            elevation: 15,
+            color: Color.fromRGBO(245, 245, 245, 1),
+            child: Container(
+              height: 100,
+              child: InCard(carddata: widget.list[index]),
+            ),
+          ),
+          ); 
+          
+           
       }
+    );
+  }
+}
+
+
+class InCard extends StatelessWidget {
+  
+
+     const InCard({this.carddata }); 
+
+
+  final String carddata;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
     );
   }
 }
